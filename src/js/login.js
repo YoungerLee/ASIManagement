65714b241f4ba569cf329007d5cfe524f359fe18
PeiYang Xie <me@xiepeiyang.com>
2016-04-21 23:13:41 +0800

    arrange src/pages

--- src/js/login.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$(".List").hover(function(){
-		$(this).children("ul").css({"display":"block","animation":"flipOutY 2s ease"});
-	},function(){
-		$(this).children("ul").css("display","none");
-	})
-});
