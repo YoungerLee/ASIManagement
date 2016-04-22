ab754dbd83f2ec24797ecc8b91e1e831fe4ff52b
YoungerLee <651238355@qq.com>
2016-04-22 22:02:20 +0800

    web.xml configuration and remove .project

--- src/ASIManagement/src/com/asi/manage/web/action/DemoAction.java
+++ src/ASIManagement/src/com/asi/manage/web/action/DemoAction.java
@@ -2,9 +2,19 @@ package com.asi.manage.web.action;
 
 import javax.annotation.Resource;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
 import com.asi.manage.domain.Demo;
 import com.asi.manage.service.DemoService;
 
+/**
+ * @Controller("demoAction") 相当于spring容器中定义 <bean id="demoAction"
+ *                           class="com.asi.manage.web.action.DemoAction"
+ *                           scope="prototype">
+ */
+@Controller("demoAction")
+@Scope(value = "prototype")
 @SuppressWarnings("serial")
 public class DemoAction extends BaseAction<Demo> {
 	private Demo demo = this.getModel();
